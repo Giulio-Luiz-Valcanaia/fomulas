5efe7cd3c78e5673b92f0c71e80b08fb41fb1c18
Giulio-Luiz-Valcanaia <67170840+Giulio-Luiz-Valcanaia@users.noreply.github.com>
2022-05-21 09:55:29 -0300

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,71 @@
+minetest.register_node("formula:one_high_two", {
+    description = "1^2",
+    tiles = {"one_high_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:one_high_three", {
+    description = "1^3",
+    tiles = {"one_high_three.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:two_high_two", {
+    description = "2^2",
+    tiles = {"two_high_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:two_high_three", {
+    description = "2^3",
+    tiles = {"two_high_three.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:three_high_two", {
+    description = "3^2",
+    tiles = {"three_high_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:three_high_three", {
+    description = "3^3",
+    tiles = {"three_high_three.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:four_high_two", {
+    description = "4^2",
+    tiles = {"four_high_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:four_high_three", {
+    description = "4^3",
+    tiles = {"four_high_three.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:five_high_two", {
+    description = "5^2",
+    tiles = {"five_high_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:five_high_three", {
+    description = "5^3",
+    tiles = {"five_high_three.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:six_high_two", {
+    description = "6^2",
+    tiles = {"six_high_two.png"},
+    groups = {cracky = 1}
+})
+
+minetest.register_node("formula:six_high_three", {
+    description = "6^3",
+    tiles = {"six_high_three.png"},
+    groups = {cracky = 1}
+})
